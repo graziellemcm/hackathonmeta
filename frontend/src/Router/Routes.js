73ce1b14ca6280ecdaa3113a808c1f0e69a39884
francine1919 <francine1919@gmail.com>
2022-05-27 15:06:25 -0300

    Merge branch 'francine-dev' into development

--- frontend/src/Router/Routes.js
+++ frontend/src/Router/Routes.js
@@ -4,10 +4,6 @@ import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
-import AboutUsPage from "../Pages/AboutUsPage/AboutUsPage";
-import AgroWalletPage from "../Pages/AgroWalletPage/AgroWalletPage";
-import CarWalletPage from "../Pages/CarWalletPage/CarWalletPage";
-
 
 export default function Router() {
   return (
@@ -17,9 +13,6 @@ export default function Router() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<SignUp />} />
-          <Route exact path="/aboutus" element={<AboutUsPage />} />
-          <Route exact path="/agro/user/wallet" element={<AgroWalletPage />} />
-          <Route exact path="/car/user/wallet" element={<CarWalletPage />} />
           <Route exact path="*" element={<ErrorPage />} />
         </Routes>
       </BrowserRouter>
