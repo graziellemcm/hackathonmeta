dc6578815a496e7c0e67c04ef1419ade3dddef85
francine1919 <francine1919@gmail.com>
2022-06-03 17:21:45 -0300

    remove token from form

--- backend/src/controller/FeedbackController.ts
+++ backend/src/controller/FeedbackController.ts
@@ -7,7 +7,6 @@ export class FeedbackController {
   async createFeedback(req: Request, res: Response) {
     try {
       //inputs req
-      const token_headers = req.headers.authorization as string;
       const input: FeedbackInputDTO = {
         email_leaguer: req.body.email_leaguer,
         email_creator: req.body.email_creator,
@@ -16,11 +15,10 @@ export class FeedbackController {
 
       //creating feedback request in databank
       const feedbackBusiness = new FeedbackBusiness();
-      const token = await feedbackBusiness.createFeedback(input, token_headers);
+      await feedbackBusiness.createFeedback(input);
       await transporter(input.email_evaluators);
       res.status(201).send({
         message: "Avaliação criada!",
-        token: token,
         email_creator: input.email_creator,
         email_leaguer: input.email_leaguer,
       });
