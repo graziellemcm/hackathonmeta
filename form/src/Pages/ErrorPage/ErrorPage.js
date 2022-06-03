d22a2fceafd3d420043c06c1a01b480025838b0a
francine1919 <francine1919@gmail.com>
2022-06-02 22:11:42 -0300

    form

---
+++ form/src/Pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+function ErrorPage() {
+  return (
+    <div>
+      <iframe
+        width="1356"
+        height="400"
+        allowFullScreen
+        src="https://giphy.com/embed/qjgm2rlJ6wep88aitp"
+      />
+    </div>
+  );
+}
+export default ErrorPage;
