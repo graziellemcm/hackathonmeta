5d095b9a4c24b74a17ed163882fe13c3a33c21a3
udimile <76756821+udimile@users.noreply.github.com>
2022-06-06 00:53:27 -0300

    leaguerProfile

--- frontend/src/Pages/LeaguerProfile/LeaguerProfile.js
+++ frontend/src/Pages/LeaguerProfile/LeaguerProfile.js
@@ -7,8 +7,8 @@ import star from "../../Components/img/star.png"
 import Vector from "../../Components/img/Vector.png"
 import Labs from "../../Components/img/Labs.png"
 import Mentor from "../../Components/img/Mentor.png"
-import { Avatar, Button } from "@mui/material";
-import { goEditLeaguer, goToNewEvaluation } from "../../Router/coordinator";
+import { Avatar, Button, CircularProgress, Rating, Typography } from "@mui/material";
+import { goEditLeaguer, goToCompilation, goToNewEvaluation } from "../../Router/coordinator";
 import { useNavigate, useParams } from "react-router-dom";
 import useRequestData from "../../Hooks/useRequestData";
 import { useProtectedPage } from "../../Hooks/useProtectedPage";
@@ -18,28 +18,36 @@ export default function LeaguerProfile() {
     useProtectedPage()
     const navigate = useNavigate()
     const params = useParams();
-    const [leaguerData, loading] = useRequestData([], `${base_Url}/leaguer/get/${params.id}`)
-    const [evaluationData, loadingEvaluation]= useGetEvaluation(leaguerData.email)
-    console.log(evaluationData)
-    
+    const [leaguerData, loadingLeaguer] = useRequestData({}, `${base_Url}/leaguer/get/${params.id}`)
+    const data = useRequestData([], `${base_Url}/evaluation/leaguer/${params.id}`)[0]
+    const [averaged, loadingAverage] = useRequestData({}, `${base_Url}/evaluation/leaguer/averaged/${params.id}`)
+
+    const [compiled, loadingCompiled] = useRequestData([], `${base_Url}/compiled/get/${params.id}`)
+
+    console.log(compiled)
+
     return (
 
         <div>
             <Header />
             <HeaderProfile>
-                <Button >INICIO</Button>
-                <div>
-                    <Button >HISTORICO</Button>
-                    <Button
-                    onClick={()=>{
-                        goEditLeaguer(navigate,params.id)
-                    }}
-                    >EDITAR LEAGUER</Button>
-                    <Button
-                        onClick={() => goToNewEvaluation(navigate)}
 
-                    >  Criar nova avaliação</Button>
-                </div>
+                <Button
+                    onClick={() => {
+                        goEditLeaguer(navigate, params.id)
+                    }}
+                >EDITAR LEAGUER</Button>
+                <Button
+                    onClick={() => goToNewEvaluation(navigate)}
+
+                >  Criar nova avaliação</Button>
+                <Button
+
+                    onClick={() => {
+                        goToCompilation(navigate, params.id)
+                    }}
+                >Compilação
+                </Button>
 
             </HeaderProfile>
 
@@ -49,10 +57,10 @@ export default function LeaguerProfile() {
                 <NameProfile>{leaguerData.name}</NameProfile>
                 <TeamProfile> <TeamImg src={Vector}></TeamImg>{leaguerData.class_name}</TeamProfile>
                 <PhaseProfile> <Star src={Labs} ></Star> {leaguerData.phase}</PhaseProfile>
-                
-               
 
-               
+
+
+
 
 
             </LeaguerCardHeader>
@@ -108,65 +116,37 @@ export default function LeaguerProfile() {
                 </CardProfileLeaguer>
 
 
-                <CardWorkingSince>
-                    <CenterTitle>  <h5>Media dos ultimos x tempo</h5></CenterTitle>
-                    <ULLeaguer>
-                        Performace na entrega
-                        <Star src={star}></Star>
-                    </ULLeaguer>
 
-                    <ULLeaguer>
-                        Qualidade da entrega
-                        <Star src={star}></Star>
-                    </ULLeaguer>
-
-                    <ULLeaguer>
-                        Comprometimento
-                        <div>
-                            <Star src={star}></Star>
-                            <Star src={star}></Star>
-                            <Star src={star}></Star>
-                            <Star src={star}></Star>
+                {/* {loadingAverage ? <CircularProgress sx={{ m: "40vh auto" }} /> : 
+                    <CardWorkingSince>
+                <Typography variant="h2" fontSize={20} sx={{ m: 1, }} >Média das avaliações quantitativas</Typography> 
+                    <div>
+                        <Typography variant="h2" fontSize={16}>Desempenho:</Typography>
+                        <Rating name="performance" defaultValue={averaged.compiled.performance} precision={0.25} readOnly />
+                        <Typography variant="h2" fontSize={16}>Qualidade de entrega:</Typography>
+                        <Rating name="quality_on_delivery" defaultValue={averaged.compiled.quality_on_delivery} precision={0.25} readOnly />
+                        <Typography variant="h2" fontSize={16}>Proatividade: </Typography>
+                        <Rating name="proactivity" defaultValue={averaged.compiled.proactivity} precision={0.25} readOnly />
+                        <Typography variant="h2" fontSize={16}>Compromisso:</Typography>
+                        <Rating name="commitment" defaultValue={averaged.compiled.commitment} precision={0.25} readOnly />
+                        <Typography variant="h2" fontSize={16}>Trabalho em equipe:</Typography>
+                        <Rating name="team_work" defaultValue={averaged.compiled.team_work} precision={0.25} readOnly />
+                        <Typography variant="h2" fontSize={16}>Crescimento: </Typography>
+                        <Rating name="skillset_growth" defaultValue={averaged.compiled.skillset_growth} precision={0.25} readOnly />
+                        <Typography variant="h2" fontSize={16}>Liderança: </Typography>
+                        <Rating name="leadership" defaultValue={averaged.compiled.leadership} precision={0.25} readOnly />
+                        <Typography variant="h2" fontSize={16}>Pontualidade:</Typography>
+                        <Rating name="punctuality" defaultValue={averaged.compiled.punctuality} precision={0.25} readOnly />
+                        <Typography variant="h2" fontSize={16}>Trabalhar sob pressão: </Typography>
+                        <Rating name="work_under_pressure" defaultValue={averaged.compiled.work_under_pressure} precision={0.25} readOnly />
+                        <Typography variant="h2" fontSize={16}>Participação: </Typography>
+                        <Rating name="participation" defaultValue={averaged.compiled.participation} precision={0.25} readOnly />
+                        <Typography variant="h2" fontSize={16}>Tarefas administrativas: </Typography>
+                        <Rating name="administrative_tasks" defaultValue={averaged.compiled.administrative_tasks} precision={0.25} readOnly />
                         </div>
-                    </ULLeaguer>
+                         </CardWorkingSince>
+                    }  */}
 
-                    <ULLeaguer>
-                        Trabalho em equipe
-                        <Star src={star}></Star>
-                    </ULLeaguer>
-
-                    <ULLeaguer>
-                        Desenvolvimento de habilidades
-                        <Star src={star}></Star>
-                    </ULLeaguer>
-
-                    <ULLeaguer>
-                        Capacidade de liderança
-                        <Star src={star}></Star>
-                    </ULLeaguer>
-
-                    <ULLeaguer>
-                        Pontualidade
-                        <Star src={star}></Star>
-                    </ULLeaguer>
-
-                    <ULLeaguer>
-                        Trabalho sobre pressao
-                        <Star src={star}></Star>
-                    </ULLeaguer>
-
-                    <ULLeaguer>
-                        Participação em cerimonia
-                        <Star src={star}></Star>
-                    </ULLeaguer>
-
-                    <ULLeaguer>
-                        Atividade administrativa
-                        <Star src={star}></Star>
-                    </ULLeaguer>
-
-
-                </CardWorkingSince>
             </Layout>
 
             <Center>
